17daf4ea0be2d665f2d206e6e2ce2aed7d347a2a
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-01-27 22:56:45 +0530

    builds functional timer

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
 
@@ -7,10 +9,35 @@ class Stopwatch extends StatefulWidget {
 }
 
 class _StopwatchState extends State<Stopwatch> {
+  late DateTime _initialTime;
+  Duration elapsed = Duration.zero;
+  late final Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialTime = DateTime.now();
+    _timer = Timer.periodic(
+      Duration(milliseconds: 50),
+      (timer) {
+        final now = DateTime.now();
+        setState(() {
+          elapsed = now.difference(_initialTime);
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ElapsedTimeTextBasic(
-      elapsed: Duration(seconds: 5),
+      elapsed: elapsed,
     );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 }
