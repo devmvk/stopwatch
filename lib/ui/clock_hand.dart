7ba7b717bf8e03f81fed183023534b0b3ce8402c
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-03-15 23:31:13 +0530

    basic clock hand added

--- lib/ui/clock_hand.dart
+++ lib/ui/clock_hand.dart
@@ -4,16 +4,22 @@ class ClockHand extends StatelessWidget {
   final double height;
   final double angleRadians;
   final double width;
+  final Color color;
 
   const ClockHand({
     Key? key,
     required this.height,
     required this.angleRadians,
     required this.width,
+    required this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: width,
+      height: height,
+      color: color,
+    );
   }
 }
