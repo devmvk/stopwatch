6d03850f8cae55f860a25e9b8333e4aeb3bd28da
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-03-15 23:28:34 +0530

    creates clock hand widget

---
+++ lib/ui/clock_hand.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ClockHand extends StatelessWidget {
+  final double height;
+  final double angleRadians;
+  final double width;
+
+  const ClockHand({
+    Key? key,
+    required this.height,
+    required this.angleRadians,
+    required this.width,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
