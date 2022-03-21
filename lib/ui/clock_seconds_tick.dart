fbff5ba1984a3597cf02afedab70fcca77f874af
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-03-21 22:30:46 +0530

    created clock seconds tick

---
+++ lib/ui/clock_seconds_tick.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ClockSecondsTicker extends StatelessWidget {
+  final int seconds;
+  final double width;
+  final double height;
+
+  const ClockSecondsTicker({
+    Key? key,
+    required this.seconds,
+    required this.height,
+    required this.width,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+    );
+  }
+}
