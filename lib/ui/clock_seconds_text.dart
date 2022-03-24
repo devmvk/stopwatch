f94308921f5698a1a0330e4ebb7294d1097067bb
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-03-25 00:11:00 +0530

    applys fixed width

--- lib/ui/clock_seconds_text.dart
+++ lib/ui/clock_seconds_text.dart
@@ -1,33 +1,38 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 
 class ClockSecondsText extends StatelessWidget {
   final int seconds;
   final double height;
   final double width;
   final double radius;
+
   const ClockSecondsText({
     Key? key,
     required this.seconds,
     required this.height,
     required this.width,
     required this.radius,
-  }) : super(key: key);
+  });
+
+  String get secondsString => seconds.toString().padLeft(2, '0');
 
   @override
   Widget build(BuildContext context) {
     return Transform(
       alignment: Alignment.center,
       transform: Matrix4.identity()
-      ..translate(-width/2, -height/2, 0)
-      ..rotateZ((pi * seconds / 30) + pi)
-      ..translate(0.0, radius - 2 * height, 0.0)
-      ,
-      child: Container(
-        height: height,
-        width: width,
-        child: Text(seconds.toString()),
+        ..translate(-width, -height / 2, 0)
+        ..rotateZ((pi * seconds / 30) + pi)
+        ..translate(0.0, radius - 2 * height, 0.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TimeDigit(secondsString.substring(0,1), width: width),
+          TimeDigit(secondsString.substring(1,2), width: width)
+        ],
       ),
     );
   }
