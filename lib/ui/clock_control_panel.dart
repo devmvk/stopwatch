af47f3e07db25d769a41b490d233323e920b3474
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2022-03-31 20:56:16 +0530

    creates clock controle panel file

---
+++ lib/ui/clock_control_panel.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ClockControlPanel extends StatelessWidget {
+  const ClockControlPanel({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Spacer(),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle, color: Colors.amberAccent),
+          ),
+        ),
+        Spacer(
+          flex: 2,
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.green,
+            ),
+          ),
+        ),
+        Spacer(),
+      ],
+    );
+  }
+}
